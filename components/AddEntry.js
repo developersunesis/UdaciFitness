6b74015a59a81aa9cb7fef7f5b3e9fb013f4630e
developersunesis <developersunesis@gmail.com>
2020-08-19 09:11:15 +0100

    Initial commit

---
+++ components/AddEntry.js
@@ -0,0 +1,157 @@
+import React from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import UdaciSlider from './UdaciSlider'
+import UdaciStepper from './UdaciStepper'
+import DateHeader from './DateHeader'
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
+import { submitEntry, removeEntry } from '../utils/api'
+import { connect } from 'react-redux'
+import { addEntry } from '../actions/index'
+import { getDailyRemindervalue } from '../utils/helpers'
+
+function SubmitBtn ({ onPress }) {
+    return (
+        <TouchableOpacity
+            onPress={onPress}>
+                
+        </TouchableOpacity>
+    )
+}
+
+class AddEntry extends React.Component {
+
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0,
+    }
+
+    increment = (metric) => {
+        const { max, step } = getMetricMetaInfo(metric)
+
+        this.setState((state) => {
+            const count = state[metric] + step
+            
+            return {
+                ...state,
+                [metric] : count > max ? max : count
+            }
+        })
+    }
+
+    decrement = (metric) => {
+        this.setState((state) => {
+            const count = state[metric] - getMetricMetaInfo(metric).step
+            
+            return {
+                ...state,
+                [metric] : count < 0 ? 0 : count
+            }
+        })
+    }
+
+    slide = (metric, value) => {
+        this.setState(() => ({
+            [metric] : value
+        }))
+    }
+
+    submit = () => {
+        const key = timeToString()
+        const entry = this.state
+        
+        this.props.dispatch(addEntry({
+            [key] : entry
+        }))
+
+        this.setState(() => ({
+            run: 0,
+            bike: 0,
+            swim: 0,
+            sleep: 0,
+            eat: 0,
+        }))
+        // Navigate to home
+
+        submitEntry({key, entry});
+
+        // Clear local notification
+    }
+
+    reset = () => {
+        const key = timeToString()
+        
+        this.props.dispatch(addEntry({
+            [key] : getDailyRemindervalue(),
+        }))
+
+        // Navigate to home
+
+        removeEntry(key)
+    }
+
+    render () {
+        const metaInfo = getMetricMetaInfo()
+
+        if(this.props.alreadyLogged) {
+            return (
+                <View>
+                    <Ionicons
+                    name='md-happy'
+                    size={100}/>
+                    <Text>You already logged your information for today</Text>
+                    <TextButton onPress={this.reset}>
+                        Reset
+                    </TextButton>
+                </View>
+            )
+        }
+
+        return (
+            <View>
+                <DateHeader date={new Date().toLocaleDateString()} />
+                { 
+                    Object.keys(metaInfo).map((key) => {
+                        const {getIcon, type, ...rest} = metaInfo[key]
+                        const value = this.state[key]
+
+                        return (
+                            <View key={key}>
+                                {getIcon()}
+                                {
+                                    type === 'slider' ?
+                                    <UdaciSlider
+                                        value={value}
+                                        onChange={(value) => this.slide(key, value)}
+                                        {...rest}/>
+                                    :
+                                    <UdaciStepper
+                                        value={value}
+                                        onIncrement={() => this.increment(key)}
+                                        onDecrement={() => this.decrement(key)}
+                                        {...rest}/>
+                                }
+                            </View>
+                        )
+                    })
+                }
+
+                <SubmitBtn onPress={this.submit} />
+            </View>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    const key = timeToString()
+
+    return {
+        alreadyLogged : state[key] && typeof state[key].today === 'unudefined'
+    }
+}
+
+export default connect(mapStateToProps)(AddEntry)
